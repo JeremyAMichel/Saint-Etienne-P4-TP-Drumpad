e8d555efc37976c5a552b3ae0cbae19ca1970daa
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2024-11-27 09:57:39 +0100

    Correction part 2

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 document.addEventListener("keydown", handleKeyPress);
 document.addEventListener("keyup", handleKeyPress);
 
+let isRecording = false;
+let recordedKeys = [];
+let startRecordDate;
+
 function handleKeyPress(event) {
+  console.log(event);
+
   // Si l'event est declenché en boucle car la personne laisse la touche appuyer, on ne fait rien !
   if (event.repeat) {
     return;
@@ -15,18 +21,28 @@ function handleKeyPress(event) {
     return; // sinon on ne continue pas la fonction
   }
 
-  key.classList.toggle("playing"); // On applique la classe CSS pour l'effet visuel
-
   if (event.type === "keydown") {
-    // si on appuie sur la touche, on déclenche le son associé
-    playSound(event);
+    key.classList.toggle("playing");
   }
+
+  if (event.type === "keyup") {
+    if (event.keyCode !== 82 && event.keyCode !== 80) {
+      key.classList.toggle("playing");
+      return;
+    }
+
+    if (event.keyCode === 82) {
+      // 82 est la touche de record
+      triggerRecord();
+    }
+  }
+
+  playSound(event);
 }
 
 function playSound(event) {
-  
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-  
+
   // Directement après avoir essayé de récuperer l'audio associée à une touche, on vérifie
   // si on a bien réussi à avoir un audio
   if (!audio) {
@@ -35,4 +51,41 @@ function playSound(event) {
 
   audio.currentTime = 0; // Remet le son au début
   audio.play();
+
+  if (isRecording) {
+    recordedKeys.push({
+      keyCode: event.keyCode,
+      timeCode: Date.now() - startRecordDate,
+    });
+
+    console.log(recordedKeys);
+  }
 }
+
+function triggerRecord() {
+  isRecording = !isRecording;
+
+  if (isRecording) recordedKeys = [];
+
+  startRecordDate = Date.now();
+}
+
+// setTimeout(() => {
+//   console.log("hello au bout de 2 secondes");
+
+// }, 2000)
+
+// setInterval(() => {
+  // const newKeyBoardDownEvent = new KeyboardEvent("keydown", { keyCode: 65 });
+
+  // const newKeyBoardUpEvent = new KeyboardEvent("keyup", { keyCode: 65 });
+  // document.dispatchEvent(newKeyBoardDownEvent);
+
+  // setTimeout(() => {
+  //   document.dispatchEvent(newKeyBoardUpEvent);
+  // }, 300)
+  
+// }, 2000);
+
+// const newKeyBoardEvent = new KeyboardEvent("keydown", {keyCode: 65 });
+// document.dispatchEvent(newKeyBoardEvent);
